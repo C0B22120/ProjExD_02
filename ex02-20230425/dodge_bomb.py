f52aff0af7f898ec574ff87605207e897fdd94c9
hashiguchi <c0b22120f1@edu.teu.ac.jp>
2023-04-25 14:20:31 +0900

    問いの2の状態

--- ex02-20230425/dodge_bomb.py
+++ ex02-20230425/dodge_bomb.py
@@ -1,5 +1,7 @@
-import pygame as pg
+import random
 import sys
+
+import pygame as pg
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -8,9 +10,11 @@ def main():
     bg_img = pg.image.load("ex02-20230425/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02-20230425/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20,20))
+    bb_img = pg.Surface((20,20))  #bombの描画
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
+    bb_img.set_colorkey((0,0,0))  #爆弾の透過
+    x,y = random.randint(0,1600) , random.randint(0,900)  #座標の指定
+    screen.blit(bb_img,[x,y])
     tmr = 0
 
     while True:
@@ -21,6 +25,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img,[x,y])  #表示
 
         pg.display.update()
         clock.tick(1000)
